a8f051cb9e5b75d698739d71166dd35f902d75a2
Ines <inesagbozo@gmail.com>
2024-04-03 10:09:40 +0100

    modification

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,7 @@ module.exports = {
         orange: "#fac031",
         gray: "#ccccc",
       },
-      backgroundImage: {
-        'cuisto1': "url('./Team/cuisto1.jpg')",
-        'cuisto2': "url('./Team/cuisto2.jpg')",
-        'cuisto3': "url('./Team/cuisto3.jpg')",
-        'team':"url('./Team/background.jpeg')",
-      },
     },
   },
   plugins: [],
 }
-
