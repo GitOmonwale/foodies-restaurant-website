e814860263eb834574c3a9512d6fb3617c6b3e71
Ines <inesagbozo@gmail.com>
2024-04-08 14:38:37 +0100

    modification

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // barre de navigation responsive
- 
-const hamburger = document.querySelector(".hamburger");
+
+const hamburger = document.querySelector(".hamburger")
 const menu = document.querySelector(".menu");
 const cross = document.querySelector(".cross");
 const nav = document.querySelector(".nav-responsive");
@@ -8,6 +8,7 @@ hamburger.addEventListener('click', () => {
     menu.classList.remove('mt-[-100%]', 'hidden');
     menu.classList.add('mt-0', 'flex');
     nav.classList.remove('hidden');
+    console.log('out!!!')
 })
 
 cross.addEventListener('click', () => {
@@ -15,14 +16,13 @@ cross.addEventListener('click', () => {
     menu.classList.remove('mt-0', 'flex');
     nav.classList.add('hidden');
 })
- 
+
 
 const options = document.querySelectorAll('.option');
-options.forEach(function(option) {
-    option.addEventListener('click',()=>{
+options.forEach(function (option) {
+    option.addEventListener('click', () => {
         nav.classList.add('hidden');
     })
 });
 
 
- 
